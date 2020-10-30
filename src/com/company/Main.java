34cda0b9e2b2970a8b67d8baf6a9bc5d24e19462
hslaney <haley.slaney@gmail.com>
2020-10-30 18:31:25 -0400

    commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,8 +5,52 @@ public class Main {
     public static void main(String[] args) {
         // write your code here
         conditionals c1 = new conditionals();
+//        System.out.println("chimp trouble");
+//        System.out.println(c1.chimpTrouble(true, true));
+//        System.out.println(c1.chimpTrouble(true, false));
 //        System.out.println(c1.chimpTrouble(false, true));
-//        System.out.println(c1.negPos(-1, -1, true));
+//        System.out.println(c1.chimpTrouble(false, false));
+
+//        System.out.println("negPos");
+//        System.out.println(c1.negPos(1, -1, false));
+//        System.out.println(c1.negPos(-1, 1, false));
+//        System.out.println(c1.negPos(1, 1, false));
+//        System.out.println(c1.negPos(-1, -1, false));
+//        System.out.println(c1.negPos(-4, -5, true));
+//        System.out.println(c1.negPos(-4, 5, true));
+//        System.out.println(c1.negPos(4, 5, true));
+//        System.out.println(c1.negPos(4, -5, true));
+
+
+//        System.out.println("pick up phone");
+//        System.out.println(c1.pickUpPhone(false, false, true));
+//        System.out.println(c1.pickUpPhone(true, true, true));
 //        System.out.println(c1.pickUpPhone(true, true, false));
+//        System.out.println(c1.pickUpPhone(false, true, false));
+//        System.out.println(c1.pickUpPhone(true, false, false));
+//        System.out.println(c1.pickUpPhone(false, false, false));
+
+//        System.out.println("setAlarm");
+//        System.out.println(c1.setAlarm(1, false));
+//        System.out.println(c1.setAlarm(5, false));
+//        System.out.println(c1.setAlarm(0, false));
+//        System.out.println(c1.setAlarm(2, true));
+//        System.out.println(c1.setAlarm(4, true));
+//        System.out.println(c1.setAlarm(6, true));
+
+//        System.out.println("onesdigit");
+//        System.out.println(c1.onesDigitSame(23,19,13));
+//        System.out.println(c1.onesDigitSame(23,19,12));
+//        System.out.println(c1.onesDigitSame(23,19,3));
+//        System.out.println(c1.onesDigitSame(423,13,3));
+//        System.out.println(c1.onesDigitSame(23,29,25));
+
+
+        System.out.println("Blackjack");
+        System.out.println(c1.blackjack(19,21));
+        System.out.println(c1.blackjack(21,19));
+        System.out.println(c1.blackjack(19,22));
+        System.out.println(c1.blackjack(8,8));
+        System.out.println(c1.blackjack(25,24));
     }
 }
--- src/com/company/conditionals.java
+++ src/com/company/conditionals.java
@@ -4,42 +4,80 @@ public class conditionals {
 
 
     public boolean chimpTrouble(boolean aSmile, boolean bSmile) {
-        boolean value = aSmile && bSmile || !(aSmile||bSmile);
-        return value;
+        return aSmile && bSmile || !(aSmile || bSmile);
     }
 
-    public boolean negPos(int x, int y, boolean negative){
-        boolean first = x>0;
-        boolean second = y>0;
-        boolean value = !(first&&second) && (first||second);
-        boolean value2 = !(first&second) && negative;
-        if (value2) {
-            return value2;
+    public boolean negPos(int x, int y, boolean negative) {
+        boolean first = x > 0;
+        boolean second = y > 0;
+        boolean value = !(first && second) && (first || second) && !negative;
+
+        if (!(first || second) && negative) {
+            return true;
         }
-        else {
+        else if (value){
             return value;
         }
+        else {
+            return false;
+        }
     }
 
-    public boolean pickUpPhone(boolean isMorning, boolean isMom, boolean isAsleep){
+    public boolean pickUpPhone(boolean isMorning, boolean isMom, boolean isAsleep) {
         if (isAsleep){
-            return !isAsleep;
+            return false;
         }
-        else if (isMorning && isMom){
-            return isMom;
+        else if (isMorning && !(isMorning && isMom)){
+            return false;
         }
         else {
-            return isMorning;
-            }
+            return true;
+        }
     }
 
-    public String setAlarm(int x, boolean y){
-        boolean weekday = x>0 && x<=;
-        boolean weekend = (x<1 || x>5);
-        if (weekday && !y){
+    public String setAlarm(int x, boolean y) {
+        boolean weekday = x > 0 && x <= 5;
+        boolean weekend = (x < 1 || x >= 6);
+        if (weekday && !y)
+        {
             return "7:00";
         }
-        else if (weekday && )
+        else if (weekday && y || weekend && !y)
+        {
+            return "10:00";
+        }
+        else
+        {
+            return "off";
+        }
 
     }
+    public boolean onesDigitSame(int a, int b, int c){
+        int first = a%10;
+        int second = b%10;
+        int third = c%10;
+
+
+        if (first == second || second == third || first == third){
+            return true;
+        }
+        else{
+            return false;
+        }
+
+    }
+    public int blackjack(int x, int y){
+        int first = 21 - x;
+        int second = 21 - y;
+        if ((second - first >= first - second || y>21) && x<=21){
+            return x;
+        }
+        else if ((first - second >= second - first || x>21) && y<=21){
+            return y;
+        }
+        else{
+            return 0;
+        }
+    }
+
 }
